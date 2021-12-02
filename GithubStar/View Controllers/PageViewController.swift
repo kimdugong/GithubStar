2487b8d5f49f483417ce588a34e6437aa9440e37
Dugong <js0628@ymail.com>
2021-12-03 00:29:03 +0900

    navigation ui 수정

--- GithubStar/View Controllers/PageViewController.swift
+++ GithubStar/View Controllers/PageViewController.swift
@@ -11,9 +11,10 @@ import Pageboy
 
 class PageViewController: TabmanViewController {
     private var viewControllers: [UIViewController]
+    let scrollView: UIScrollView = UIScrollView()
     private let tabBar: TMBarView<TMHorizontalBarLayout, TMLabelBarButton, TMLineBarIndicator> = {
         let bar = TMBar.ButtonBar()
-        bar.backgroundView.style = .flat(color: .clear)
+        bar.backgroundView.style = .blur(style: .extraLight)
         bar.layout.alignment = .centerDistributed
         bar.layout.contentMode = .fit
         bar.layout.transitionStyle = .snap // Customize
@@ -22,49 +23,63 @@ class PageViewController: TabmanViewController {
             button.selectedTintColor = .darkText
         }
         bar.indicator.tintColor = .darkText
-
+        
         return bar
     }()
-
+    
     init(pages viewControllers: [UIViewController]) {
         self.viewControllers = viewControllers
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
     }
-
+    
     // MARK: - Setting Up UI
     private func setupUI() {
         title = "Example"
-
+        
         // MARK: - setup tabman
         bounces = false
         dataSource = self
         addBar(tabBar, dataSource: self, at: .top)
+        
+        let visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .extraLight))
+        view.addSubview(visualEffectView)
+        visualEffectView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.top)
+        }
+        
+//        view.addSubview(scrollView)
+//        scrollView.snp.makeConstraints { make in
+//            make.size.edges.equalToSuperview()
+//        }
+//        view.sendSubviewToBack(scrollView)
+        
     }
 }
 
 extension PageViewController: PageboyViewControllerDataSource {
     func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
-        2
+        viewControllers.count
     }
-
+    
     func viewController(for pageboyViewController: PageboyViewController, at index: PageboyViewController.PageIndex) -> UIViewController? {
         viewControllers[index]
     }
-
+    
     func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
         nil
     }
-
+    
 }
 
 extension PageViewController: TMBarDataSource {
--- GithubStar/View Controllers/RemoteViewController.swift
+++ GithubStar/View Controllers/RemoteViewController.swift
@@ -14,6 +14,7 @@ class RemoteViewController: UIViewController {
         tableView.register(UserTableViewCell.self, forCellReuseIdentifier: UserTableViewCell.identifier)
         tableView.backgroundColor = .green
         tableView.dataSource = self
+        tableView.delegate = self
         return tableView
     }()
 
@@ -32,23 +33,39 @@ class RemoteViewController: UIViewController {
 
     private func setupUI() {
         title = "dugong"
-        view.backgroundColor = .white
-
         view.addSubview(tableView)
         tableView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
     }
+    
+    
+}
+
+extension RemoteViewController: UITableViewDelegate {
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        if let parent = self.parent?.parent as? PageViewController {
+//            parent.scrollView.contentSize = scrollView.contentSize
+//            parent.scrollView.contentInset = scrollView.contentInset
+//            parent.scrollView.contentOffset = scrollView.contentOffset
+//            parent.scrollView.decelerationRate = scrollView.decelerationRate
+//            parent.scrollView.panGestureRecognizer.state = scrollView.panGestureRecognizer.state
+//            parent.scrollView.directionalPressGestureRecognizer.state = scrollView.directionalPressGestureRecognizer.state
+//        }
+//    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
 extension RemoteViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 50
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell(style: .default, reuseIdentifier: nil)
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
     }
-
-
 }
